14b8c7bff6ce2f8af447e86bbc1e2da0ce64c9d2
BrentG <93421742+BrentG45@users.noreply.github.com>
2022-10-28 19:24:35 +1300

    Add files via upload

--- gamemode.js
+++ gamemode.js
@@ -1,13 +1,18 @@
 
 
+
 let tool = new Tool("Katana")
 tool.model = 3441
 assign_int = 0
 var i
-var bricks = []
-var target_team
+var bricks = [];
+var target_team = [];
+
 function return_team(item_returning){
-  target_team = item_returning
+  if (target_team.length >= assign_int){
+    target_team = []
+  }
+  target_team.push(item_returning)
 }
 function sleep(duration) {
 	return new Promise(resolve => {
@@ -25,7 +30,7 @@ Game.on("playerJoin", (player) => {
 
   player.mouseclick(() => {
     tool.on("activated", () =>{
-      bricks[player.num][0].setPosition(player.position)
+      bricks[player.num][0].setPosition(new Vector3(player.position.x,player.position.y,player.position.z+2))
     },100)
 
   })
@@ -42,17 +47,16 @@ Game.on("playerJoin", (player) => {
       //}}
     for(i=0; i < bricks.length; i++){
   
-        bricks[i][0].touching(debounce((p) => {return_team( p.team) }, 1))
-        console.log( target_team?.name, player.team.name,target_team?.name != player.team.name,target_team?.name != undefined)
-        if (target_team?.name != undefined && target_team?.name != player.team.name ){
+        bricks[i][0].touching(debounce((p) => {return_team(p.team) }, 1))
+
+        if (target_team[i]?.name != undefined && target_team[i]?.name != player.team.name ){
             bricks[i][0].touching(debounce((p) => { p.health -= 25
             }, 1))
             
         }
         bricks[i][0].setPosition(new Vector3(0,10000+i*2,0))
+        target_team[i] = undefined
       }
-      
-    target_team = undefined
   })
 
 })
